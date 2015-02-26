60681477e86a2ceafd469516ad397aa03c391090
ChrisPaek <chrispaek@Sungs-MacBook-Pro.local>
2015-02-26 14:21:33 -0600

    created homepage by modifying home.client.controller.js and home.client.view.html

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,33 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+        $scope.alerts = [
+            {
+                icon: 'glyphicon-user',
+                colour:'btn-success',
+                total: '20,408',
+                description: 'TOTAL USERS'
+            },
+            {
+                icon: 'glyphicon-folder-open',
+                colour: 'btn-primary',
+                total: '8,382',
+                description: 'TOTAL PROJECTS'
+            },
+            {
+                icon: 'glyphicon-asterisk',
+                colour: 'btn-warning',
+                total: '527',
+                description: 'NEW PROJECTS IN 24H'
+            },
+            {
+                icon: 'glyphicon-record',
+                colour: 'btn-info',
+                total: '85,000',
+                description: 'EMAILS SENT'
+            },
+
+        ];
 	}
 ]);
