1b4ac6ed1305f3611ce8b2733ed479725f39cbb1
ChrisPaek <chrispaek@Sungs-MacBook-Pro.local>
2015-03-18 10:44:59 -0500

    routing to vote controller added in project server route

--- app/routes/projects.server.routes.js
+++ app/routes/projects.server.routes.js
@@ -3,6 +3,7 @@
 module.exports = function(app) {
 	var users = require('../../app/controllers/users.server.controller');
 	var projects = require('../../app/controllers/projects.server.controller');
+	var votes = require('../../app/controllers/votes.server.controller');
 
 	// Projects Routes
 	app.route('/projects')
@@ -13,6 +14,17 @@ module.exports = function(app) {
 		.get(projects.read)
 		.put(users.requiresLogin, projects.hasAuthorization, projects.update)
 		.delete(users.requiresLogin, projects.hasAuthorization, projects.delete);
+
+	app.route('/projects/:projectId/votes')
+		.get(votes.list)
+		.post(users.requiresLogin, votes.create);
+
+	//advanced for later
+	/*
+	app.route('/projects/:projectId/votes/:voteId')
+		.put(users.requiresLogin, votes.hasAuthorization, votes.update)
+		.delete(users.requiresLogin, votes.hasAuthorization, votes.delete);
+	*/
 
 	// Finish by binding the Project middleware
 	app.param('projectId', projects.projectByID);
