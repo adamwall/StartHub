3fc8a0618d9e66a07216fb2979332a5025cd915b
Travis Struckhoff <sandal2826@gmail.com>
2015-04-11 15:31:11 -0500

    trying to refactor image saving

--- app/routes/projects.server.routes.js
+++ app/routes/projects.server.routes.js
@@ -6,37 +6,15 @@ module.exports = function(app) {
     var comment = require('../../app/controllers/comment.server.controller');
     var multipart = require('connect-multiparty');
     var multipartMiddleware = multipart();
-    var fs = require('fs');
-    var path = require('path');
+
 
 	// Projects Routes
 	app.route('/projects')
 		.get(projects.list)
 		.post(users.requiresLogin, projects.create);
 
-    app.post('/projects/img/:projectId', multipartMiddleware, function(req, res){
-
-        var mkdirSync = function (path) {
-            try {
-                fs.mkdirSync(path);
-            } catch(e) {
-                if ( e.code != 'EEXIST' ) throw e;
-            }
-        };
-
-        var file_ = req.files.file;
-        console.log(file_);
-        console.log(req.files.file.path);
-        var projectId = req.project._id;
-        var img_dir = path.join(__dirname, '../img/' + projectId + '/');
-        mkdirSync(img_dir);
-        var img_path = path.join(img_dir, file_.originalFilename);
-        console.log(img_path);
-        fs.rename(req.files.file.path, img_path, function(err){
-            if(err) console.log('ERROr ' + err);
-            console.log('completed')
-        })
-    });
+    app.route('/projects/img/:projectId', multipartMiddleware)
+        .post(projects.saveImg);
 
 	app.route('/projects/:projectId')
 		.get(projects.read)
