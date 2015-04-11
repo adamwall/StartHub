aa47c18e92f5b66fd19ba44a7db7dfceba92c989
adamwall <nagasee9@gmail.com>
2015-04-10 22:41:21 -0500

    upload image to server

--- public/modules/projects/controllers/projects.client.controller.js
+++ public/modules/projects/controllers/projects.client.controller.js
@@ -4,11 +4,58 @@
 
 var projectsApp = angular.module('projects');
 
-projectsApp.controller('ProjectsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Projects',
-	function($scope, $stateParams, $location, Authentication, Projects) {
+projectsApp.controller('ProjectsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Projects', 'FileUploader',
+	function($scope, $stateParams, $location, Authentication, Projects, FileUploader) {
         $scope.authentication = Authentication;
 
+        var uploader = $scope.uploader = new FileUploader();
+        // FILTERS
+        var logo;
+        uploader.filters.push({
+            name: 'imageFilter',
+            fn: function(item /*{File|FileLikeObject}*/, options) {
+                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        });
 
+        // CALLBACKS
+
+        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+            console.info('onWhenAddingFileFailed', item, filter, options);
+        };
+        uploader.onAfterAddingFile = function(fileItem) {
+            logo = fileItem;
+        };
+        uploader.onAfterAddingAll = function(addedFileItems) {
+            console.info('onAfterAddingAll', addedFileItems);
+        };
+        uploader.onBeforeUploadItem = function(item) {
+            console.info('onBeforeUploadItem', item);
+        };
+        uploader.onProgressItem = function(fileItem, progress) {
+            console.info('onProgressItem', fileItem, progress);
+        };
+        uploader.onProgressAll = function(progress) {
+            console.info('onProgressAll', progress);
+        };
+        uploader.onSuccessItem = function(fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+        };
+        uploader.onErrorItem = function(fileItem, response, status, headers) {
+            console.info('onErrorItem', fileItem, response, status, headers);
+        };
+        uploader.onCancelItem = function(fileItem, response, status, headers) {
+            console.info('onCancelItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteItem = function(fileItem, response, status, headers) {
+            console.info('onCompleteItem', fileItem, response, status, headers);
+        };
+        uploader.onCompleteAll = function() {
+            console.info('onCompleteAll');
+        };
+
+        console.info('uploader', uploader);
 
         // Create new Project
         $scope.create = function() {
@@ -22,6 +69,13 @@ projectsApp.controller('ProjectsController', ['$scope', '$stateParams', '$locati
 
             // Redirect after save
             project.$save(function(response) {
+                uploader.onBeforeUploadItem = function(item) {
+                    console.log('before upload')
+                    uploader.url = '/projects/img/' + response._id;
+                    item.url = '/projects/img/' + response._id;
+                };
+                console.log(uploader);
+                uploader.uploadItem(uploader.queue[0]);
                 $location.path('projects/' + response._id);
 
                 // Clear form fields
@@ -107,4 +161,3 @@ projectsApp.controller('ProjectsController', ['$scope', '$stateParams', '$locati
 */
     }
 ]);
-
