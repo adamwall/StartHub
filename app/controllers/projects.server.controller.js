8920a5c72f1f56472a2d786302d7d763c3aa9929
Travis Struckhoff <sandal2826@gmail.com>
2015-04-25 16:19:51 -0500

    got rid of aws

--- app/controllers/projects.server.controller.js
+++ app/controllers/projects.server.controller.js
@@ -10,10 +10,7 @@ var mongoose = require('mongoose'),
 
 var fs = require('fs');
 var path = require('path');
-var aws = require('aws-sdk');
-var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
-var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-var S3_BUCKET = 'starthub.img';
+
 
 /**
  * Create a Project
@@ -162,29 +159,3 @@ exports.getFileName = function(req, res, next, fileName) {
     req.fileName = fileName;
     next();
 };
-
-
-/*exports.get_aws_sign = function(req, res){
-        aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
-        var s3 = new aws.S3();
-        var s3_params = {
-            Bucket: S3_BUCKET,
-            Key: req.query.s3_object_name,
-            Expires: 60,
-            ContentType: req.query.s3_object_type,
-            ACL: 'public-read'
-        };
-        s3.getSignedUrl('putObject', s3_params, function(err, data){
-            if(err){
-                console.log(err);
-            }
-            else{
-                var return_data = {
-                    signed_request: data,
-                    url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.s3_object_name
-                };
-                res.write(JSON.stringify(return_data));
-                res.end();
-            }
-        });
-});*/
